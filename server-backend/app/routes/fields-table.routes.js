02dcd8d69a302b7bed2d039f59726d2f92ac70b1
Will Duncan <duncanbullet@gmail.com>
2021-06-01 16:49:43 -0500

    alt phone
    alt phone stuff api works now

--- server-backend/app/routes/fields-table.routes.js
+++ server-backend/app/routes/fields-table.routes.js
@@ -1,12 +1,14 @@
 module.exports = app => {
     const fields = require("../controllers/fields-table.controller");
+    const fieldAltPhone = require("../controllers/fields-controllers/field-altPhone.controller.js");
 
     var router = require("express").Router();
 
     //submit new authentication request
     router.get("/all", fields.getAllFields);
 
-    //submit user authZ 
+    //alt phone field data
+    router.get("/alt_phone", fieldAltPhone.findAll)
     
 
 
